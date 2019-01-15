a233121fe7fbeeabea8d37d2d7c68ede23acb4c5
yveso <yveso@users.noreply.github.com>
2019-01-15 21:06:21 +0100

    Docker container for API

--- containers/api/api/__init__.py
+++ containers/api/api/__init__.py
@@ -1,10 +1,14 @@
+import os
 from flask import Flask
 
 
 def create_app():
     app = Flask(__name__)
+    app_settings = os.getenv("APP_SETTINGS")
+    app.config.from_object(app_settings)
 
     from api.sanity import bp as sanity_bp
+
     app.register_blueprint(sanity_bp)
 
     return app
