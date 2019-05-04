720b040dbd5a6ea03869ca9bd29db46ed3a84357
yveso <yveso@users.noreply.github.com>
2019-05-04 23:17:15 +0200

    Deploy it!

--- containers/api/app.py
+++ containers/api/app.py
@@ -1,1 +1,6 @@
-from api import create_app  # noqa
+from waitress import serve
+from api import create_app
+
+if __name__ == "__main__":
+    app = create_app()
+    serve(app, host='0.0.0.0', port=5000)
