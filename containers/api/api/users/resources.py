0aaae2d94b1e81dbca9d40a3a8964561c37cbcef
yveso <yveso@users.noreply.github.com>
2019-05-04 18:19:32 +0200

    Users route

--- containers/api/api/users/resources.py
+++ containers/api/api/users/resources.py
@@ -1,5 +1,6 @@
 from flask import request
 from flask_restful import Api, Resource
+from sqlalchemy import exc
 
 from api import db
 from api.models import User
@@ -14,15 +15,64 @@ class UsersPing(Resource):
 
 
 class Users(Resource):
+    def get(self):
+        return (
+            {
+                "status": "success",
+                "data": {
+                    "users": [user.to_json() for user in User.query.all()]
+                },
+            },
+            200,
+        )
+
     def post(self):
         post_data = request.get_json()
+
+        if (
+            not post_data
+            or "username" not in post_data
+            or "email" not in post_data
+        ):
+            return {"message": "Invalid payload", "status": "fail"}, 400
+
         username = post_data.get("username")
         email = post_data.get("email")
-        db.session.add(User(username=username, email=email))
-        db.session.commit()
 
-        return {"status": "success", "message": f"{email} was added!"}, 201
+        try:
+            user_with_mail = User.query.filter_by(email=email).first()
+            if not user_with_mail:
+                db.session.add(User(username=username, email=email))
+                db.session.commit()
+                return (
+                    {"status": "success", "message": f"{email} was added!"},
+                    201,
+                )
+            else:
+                return (
+                    {"status": "fail", "message": "Email already exists."},
+                    400,
+                )
+        except exc.IntegrityError:
+            db.session.rollback()
+            return {"message": "Database error", "status": "fail"}, 400
+
+
+class UsersId(Resource):
+    def get(self, user_id):
+        error = {"status": "fail", "message": "User does not exist."}
+        if not str(user_id).isdigit():
+            return error, 404
+        try:
+            user = User.query.filter_by(id=user_id).first()
+            if not user:
+                return error, 404
+            else:
+                return ({"status": "success", "data": user.to_json()}, 200)
+        except ValueError:
+            return error, 404
 
 
 api.add_resource(UsersPing, "/users/ping")
 api.add_resource(Users, "/users")
+api.add_resource(UsersId, "/users/<user_id>")
--- containers/api/tests/users/test_users.py
+++ containers/api/tests/users/test_users.py
@@ -1,7 +1,72 @@
 import json
 
+from api import db
+from api.models import User
+
+
+def add_user(username, email):
+    user = User(username=username, email=email)
+    db.session.add(user)
+    db.session.commit()
+    return user
+
+
+def test_users_get(app):
+    """Get all users"""
+    client = app.test_client()
+
+    add_user("Test", "test@test.com")
+    add_user("Test2", "foo@bar.com")
+
+    response = client.get("/users")
+    data = json.loads(response.data.decode())
+
+    assert response.status_code == 200
+    assert "success" in data["status"]
+    assert len(data["data"]["users"]) == 2
+
+
+def test_users_get_id(app):
+    """Get a single user"""
+    client = app.test_client()
+
+    user = add_user("Test", "test@test.com")
+
+    response = client.get(f"/users/{user.id}")
+    data = json.loads(response.data.decode())
+
+    assert response.status_code == 200
+    assert "success" in data["status"]
+    assert "Test" in data["data"]["username"]
+    assert "test@test.com" in data["data"]["email"]
+
+
+def test_users_get_id_no_valid_id(app):
+    """Get a single user, no valid id"""
+    client = app.test_client()
+
+    response = client.get("/users/not_a_id")
+    data = json.loads(response.data.decode())
+
+    assert response.status_code == 404
+    assert "fail" in data["status"]
+    assert "User does not exist" in data["message"]
+
+
+def test_users_get_id_unknown_id(app):
+    """Get a single user, unknown id"""
+    client = app.test_client()
+
+    response = client.get("/users/999999")
+    data = json.loads(response.data.decode())
+
+    assert response.status_code == 404
+    assert "fail" in data["status"]
+    assert "User does not exist" in data["message"]
+
 
 def test_users_post(app):
+    """Post a new user"""
     client = app.test_client()
 
     response = client.post(
@@ -14,3 +79,74 @@ def test_users_post(app):
     assert response.status_code == 201
     assert "john@doe.org was added!" in data["message"]
     assert "success" in data["status"]
+
+
+def test_users_post_empty_payload(app):
+    """Post a new user with empty payload
+    Error is thrown"""
+    client = app.test_client()
+
+    response = client.post(
+        "/users", data=json.dumps({}), content_type="application/json"
+    )
+    data = json.loads(response.data.decode())
+
+    assert response.status_code == 400
+    assert "Invalid payload" in data["message"]
+    assert "fail" in data["status"]
+
+
+def test_users_post_no_username(app):
+    """Post a new user with no username in payload
+    Error is thrown"""
+    client = app.test_client()
+
+    response = client.post(
+        "/users",
+        data=json.dumps({"email": "john@doe.org"}),
+        content_type="application/json",
+    )
+    data = json.loads(response.data.decode())
+
+    assert response.status_code == 400
+    assert "Invalid payload" in data["message"]
+    assert "fail" in data["status"]
+
+
+def test_users_post_no_email(app):
+    """Post a new user with no email in payload
+    Error is thrown"""
+    client = app.test_client()
+
+    response = client.post(
+        "/users",
+        data=json.dumps({"username": "John Doe"}),
+        content_type="application/json",
+    )
+    data = json.loads(response.data.decode())
+
+    assert response.status_code == 400
+    assert "Invalid payload" in data["message"]
+    assert "fail" in data["status"]
+
+
+def test_users_post_email_twice(app):
+    """Post same email twice
+    Error is thrown"""
+    client = app.test_client()
+
+    client.post(
+        "/users",
+        data=json.dumps({"username": "John Doe", "email": "john@doe.org"}),
+        content_type="application/json",
+    )
+    response = client.post(
+        "/users",
+        data=json.dumps({"username": "John Doe2", "email": "john@doe.org"}),
+        content_type="application/json",
+    )
+    data = json.loads(response.data.decode())
+
+    assert response.status_code == 400
+    assert "Email already exists." in data["message"]
+    assert "fail" in data["status"]
