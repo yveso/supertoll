2762ebe87ccd0891eec02558f5f2e965710419bc
yveso <yveso@users.noreply.github.com>
2019-05-26 10:41:28 +0200

    Refactored models 💾

--- containers/api/tests/models/test_user.py
+++ containers/api/tests/models/test_user.py
@@ -5,7 +5,7 @@ from api.models import User
 from helpers import add_user
 
 
-def test_user_just_add_one(client):
+def test_just_add_one(client):
     user = add_user(username="Test", email="test@test.org")
     assert user.id is not None
     assert user.username == "Test"
@@ -14,25 +14,25 @@ def test_user_just_add_one(client):
     assert user.password_hash is not None
 
 
-def test_user_to_json(client):
+def test_to_json(client):
     user = add_user(username="Test", email="test@test.org")
 
     assert isinstance(user.to_json(), dict)
 
 
-def test_user_no_duplicate_username(client):
+def test_no_duplicate_username(client):
     with pytest.raises(IntegrityError):
         add_user(username="Test", email="test@test.org")
         add_user(username="Test", email="test2@test.org")
 
 
-def test_user_no_duplicate_email(client):
+def test_no_duplicate_email(client):
     with pytest.raises(IntegrityError):
         add_user(username="Test", email="test@test.org")
         add_user(username="Test2", email="test@test.org")
 
 
-def test_user_password_hashes_are_randon(client):
+def test_password_hashes_are_randon(client):
     user1 = add_user(
         username="Test 1", email="test1@test.org", password="this_is_equal"
     )
@@ -42,7 +42,7 @@ def test_user_password_hashes_are_randon(client):
     assert user1.password_hash != user2.password_hash
 
 
-def test_user_encode_auth_token(client):
+def test_encode_auth_token(client):
     user = add_user(
         username="Test", email="test@test.org", password="password123"
     )
@@ -50,7 +50,7 @@ def test_user_encode_auth_token(client):
     assert isinstance(auth_token, bytes)
 
 
-def test_user_decode_auth_token(client):
+def test_decode_auth_token(client):
     user = add_user(
         username="Test", email="test@test.org", password="password123"
     )
